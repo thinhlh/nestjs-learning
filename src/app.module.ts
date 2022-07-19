3a9a963407740829e91915eaca4073612ff3c81c
Hoang Thinh <jonhnathankevin@yahoo.com.vn>
2022-07-19 16:17:48 +0700

    [FEAT] env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [AppService],
   controllers: [AppController],
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: +process.env.DATABASE_PORT,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    autoLoadEntities: true,
-    synchronize: process.env.STAGE === 'development' ? true : false, // Should set to false for production
-  }), BookModule],
+
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        `./env/${process.env.NODE_ENV}.env`,
+    }),
+    , TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.STAGE === 'development' ? true : false, // Should set to false for production
+    }), BookModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
